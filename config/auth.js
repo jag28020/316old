28242179c8a54b14473d661e5afa1b67e738ac4d
Jake Greenstein <jag28020@gmail.com>
2017-04-23 19:38:40 -0400

    callback

--- config/auth.js
+++ config/auth.js
@@ -3,12 +3,12 @@ module.exports = {
     'facebookAuth' : {
         'clientID'      : '1702327573422105', // your App ID
         'clientSecret'  : 'c54309e40f12011c8973ba6e5b4f68f7', // your App Secret
-        'callbackURL'   : 'http://kelsieann.herokuapp.com/auth/facebook/callback'
+        'callbackURL'   : 'http://www.316beauty.io/auth/facebook/callback'
     },
 
     'instagramAuth' : {
         'clientID'      : '4e36d4b85e4b46acb846df1341d238c9', // your App ID
         'clientSecret'  : '44176a97edd94e6b8d29987471a04472', // your App Secret
-        'callbackURL'   : 'http://kelsieann.herokuapp.com/auth/instagram/callback'
+        'callbackURL'   : 'http://www.316beauty.io/auth/instagram/callback'
     },
 };
